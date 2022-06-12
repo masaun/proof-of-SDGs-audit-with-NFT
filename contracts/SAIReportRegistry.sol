5f08227b163a000a3b9757df967c0cc584c7a456
masaun <masanoriuno@gmail.com>
2022-06-12 15:09:55 +0200

    [SC]: Fix import path of modules in /libraries

--- contracts/SAIReportRegistry.sol
+++ contracts/SAIReportRegistry.sol
@@ -1,12 +1,22 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
+import { ISAIReportRegistry } from "./interfaces/ISAIReportRegistry.sol";
+
+import { Events } from './libraries/Events.sol';
+import { Helpers } from './libraries/Helpers.sol';
+import { Constants } from './libraries/Constants.sol';
+import { DataTypes } from './libraries/DataTypes.sol';
+import { Errors } from './libraries/Errors.sol';
+
 import "hardhat/console.sol";
-import "./interfaces/ISAIReportRegistry.sol";
+
 
 contract SAIReportRegistry is ISAIReportRegistry {
 
     string private something;
+
+    mapping (address => mapping (address => DataTypes.SAIReport))) saiReports; 
 
     constructor(string memory _something) {
         //[TODO]:
@@ -18,8 +28,11 @@ contract SAIReportRegistry is ISAIReportRegistry {
      * @notice - Register a new SAIReport
      * @dev - Multi-Sig btw organization and auditor
      */ 
-    function registerNewSAIReport(address organization, address auditor, string memory contentHashOfSAIReport) external override {
+    function registerNewSAIReport(address _organization, address _auditor, string memory _contentHashOfSAIReport) external override {
         // [TODO]:
+
+        DataTypes.SAIReport storage saiReport = saiReports[_organization][_auditor];
+        saiReport.contentHashOfSAIReport = _contentHashOfSAIReport;
     }
 
     function getSAIReport(address organization) external override view returns (string memory _contentHashOfSAIReport) {
